71b160c1d64fb57f361baf7f0fdc4950b28664f7
Mathew McHugh <mathmch@gmail.com>
2017-06-05 08:09:53 -0700

    Add completed files

---
+++ hash_table_chaining.py
@@ -0,0 +1,116 @@
+import linked_list
+
+# a HashTable is a array list and a number of elements in the table
+class HashTable:
+    def __init__(self, lst, items, collisions):
+        self.lst = lst
+        self.items = items
+        self.table_size = len(lst)
+        self.collisions = collisions
+
+    def __eq__(self, other):
+        return (type(other) == HashTable and
+                self.items == other.items and
+                self.lst == other.lst and
+                self.collisions == other.collisions)
+
+    def __repr__(self):
+        return ("Table Size: {}, Num of Items: {}, Collisions: {}, Table: {}"
+                .format(self.table_size, self.items, self.collisions, self.lst))
+
+
+# -> HashTable
+# Creates an empty HashTable
+def empty_hash_table():
+    empty_lst = [None] * 8
+    return HashTable(empty_lst, 0, 0)
+
+# HashTable -> HashTable
+# Doubles the length of the table and rehashes every value
+def rehash(table):
+    new_lst = [None]*(table.table_size*2)
+    for idx in range(len(table.lst)):
+        new_lst[idx] = table.lst[idx]
+    table.lst = [None]*(table.table_size*2)
+    table.collisions = 0
+    table.items = 0
+    table.table_size = table.table_size*2
+    for idx in range(len(new_lst)):
+        if new_lst[idx] != None:
+            linked = new_lst[idx]
+            for i in range(linked_list.length(linked)):
+                pair = linked_list.get(linked, i)
+                insert(table, pair[0], pair[1])
+    return table
+
+# HashTable value value -> HashTable
+#
+def insert(table, key, value):
+    index = hash(key)%table.table_size
+    if table.lst[index] is None:
+        table.lst[index] = linked_list.add(table.lst[index], 0, (key, value))
+        table.items += 1
+    else:
+        dup = False
+        linked = table.lst[index]
+        for i in range(linked_list.length(linked)):
+            if linked_list.get(linked, i)[0] == key:
+                table.lst[index] = linked_list.set(linked, i, (key, value))
+                dup = True
+        if dup == False:
+            table.collisions += 1
+            table.lst[index] = linked_list.add(linked, 0, (key, value))
+            table.items += 1
+    if (table.items/ table.table_size) > 1.5:
+        rehash(table)
+    return table
+
+# HashTable value -> value
+# Takes a HashTable and returns the value at the given key, raise error if key is not in the table
+def get(table, key):
+    index = hash(key) % table.table_size
+    if table.lst[index] is None:
+        raise LookupError
+    else:
+        linked = table.lst[index]
+        for i in range(linked_list.length(table.lst[index])):
+            if linked_list.get(linked, i)[0] == key:
+                return linked_list.get(linked, i)
+    raise LookupError
+
+# HashTable value -> HashTable
+# removes the value associated with the key, raises error if no key exists in table
+def remove(table, key):
+    index = hash(key) % table.table_size
+    if table.lst[index] is None:
+        raise LookupError
+    elif table.lst[index].first[0] == key:
+        val, table.lst[index] = linked_list.remove(table.lst[index], 0)
+        table.items -= 1
+    else:
+        linked = table.lst[index]
+        removed = False
+        for i in range(linked_list.length(linked)):
+            if linked_list.get(linked, i)[0] == key:
+                val, table.lst[index] = linked_list.remove(table.lst[index], i)
+                table.items -= 1
+                table.collisions -= 1
+                removed = True
+        if removed == False:
+            raise LookupError
+    return table
+
+# HashTable -> Int
+# Returns the number of items in the table
+def size(table):
+    return table.items
+
+# HashTable -> Float
+# Returns the load factor of the table
+def load_factor(table):
+    return table.items / table.table_size
+
+# HashTable -> Int
+# returns the number of collisions in the table
+def collisions(table):
+    return table.collisions
---
+++ hash_table_chaining_tests.py
@@ -0,0 +1,61 @@
+import unittest
+from hash_table_chaining import *
+
+class Hash_Tests(unittest.TestCase):
+
+    def test_table(self):
+        my_table = empty_hash_table()
+        self.assertEqual(my_table, HashTable([None]*8, 0, 0))
+        self.assertEqual(repr(my_table),
+                         'Table Size: 8, Num of Items: 0, Collisions: 0, Table: '
+                         '[None, None, None, None, None, None, None, None]')
+
+    def test_insert(self):
+        my_table = empty_hash_table()
+        insert(my_table, 5, 10)
+        insert(my_table, 13, 16)
+        self.assertEqual(my_table.lst[5], linked_list.Pair((13, 16), linked_list.Pair((5, 10), None)))
+        self.assertEqual(my_table.items, 2)
+        insert(my_table, 13, 8)
+        self.assertEqual(my_table.lst[5], linked_list.Pair((13, 8), linked_list.Pair((5, 10), None)))
+        self.assertEqual(my_table.items, 2)
+        self.assertEqual(my_table.collisions, 1)
+        insert(my_table, 21, 18)
+        self.assertEqual(my_table.collisions, 2)
+
+    def test_remove1(self):
+        my_table = empty_hash_table()
+        self.assertRaises(LookupError, remove, my_table, 4)
+        insert(my_table, 5, 10)
+        insert(my_table, 13, 16)
+        remove(my_table, 5)
+        self.assertEqual(my_table.lst[5], linked_list.Pair((13, 16), None))
+        self.assertRaises(LookupError, remove, my_table, 5)
+
+    def test_size(self):
+        my_table = empty_hash_table()
+        self.assertEqual(size(my_table), 0)
+        insert(my_table, 5, 10)
+        insert(my_table, 13, 16)
+        self.assertEqual(size(my_table), 2)
+
+    def test_load(self):
+        my_table = empty_hash_table()
+        self.assertEqual(load_factor(my_table), 0)
+        insert(my_table, 'hey', 2)
+        for i in range(14):
+            insert(my_table, i, 2*i)
+        insert(my_table, 'hello', 2)
+        insert(my_table, 'hey', 2)
+        insert(my_table, 'hi', 2)
+        self.assertEqual(load_factor(my_table), 17/16)
+
+    def test_collisions(self):
+        my_table = empty_hash_table()
+        self.assertEqual(collisions(my_table), 0)
+        insert(my_table, 5, 10)
+        insert(my_table, 13, 16)
+        self.assertEqual(collisions(my_table), 1)
+
+if __name__ == '__main__':
+    unittest.main()
